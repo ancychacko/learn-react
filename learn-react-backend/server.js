3e65af4d84617752398cc40314a5eb117588f1f2
Ancy Chacko <chackoancy5@gmail.com>
2026-01-01 16:33:22 +0530

    feat: Update CORS allowed origins for improved access control

--- learn-react-backend/server.js
+++ learn-react-backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 // -----------------------------
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://192.168.2.80:3000",
+  "http://192.168.2.103:3000",
   "http://172.16.2.91:3000",
 ];
 if (process.env.CORS_ORIGINS) {
