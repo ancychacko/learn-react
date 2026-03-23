796000a32a329d795ed60eeb955a6625ae53654d
Ancy Chacko <chackoancy5@gmail.com>
2026-03-23 16:42:53 +0530

    fix: Update CORS allowed origin to correct IP address

--- learn-react-backend/server.js
+++ learn-react-backend/server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:3000",
   "http://192.168.2.103:3000",
-  "http://172.16.2.62:3000",
+  "http://172.16.2.164:3000",
 ];
 if (process.env.CORS_ORIGINS) {
   process.env.CORS_ORIGINS.split(",").forEach((o) =>
